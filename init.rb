bdac60726ce759e258a2a8b024f42c76a687dd4a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-04-11 09:08:52 -0300

    Plugin "notifyme". Refs #229.

---
+++ init.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+require 'redmine'
+
+Redmine::Plugin.register :notifyme do
+  name 'Notify me'
+  author 'Eduardo Henrique Bogoni'
+  description 'Notificações'
+  version '0.1.0'
+end
