490b8d5b4d5d8d2656173d47b4077d29a1fd7713
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-06-07 13:42:37 -0300

    Suporte a associação de usuários a chats Telegram. Refs #312.

--- app/controllers/telegram_chats_controller.rb
+++ app/controllers/telegram_chats_controller.rb
@@ -5,6 +5,8 @@ class TelegramChatsController < ApplicationController
   active_scaffold :telegram_chat do |conf|
     conf.list.columns << :chat_id
     conf.show.columns << :chat_id
-    conf.actions.exclude :create, :update, :delete
+    conf.columns[:user].form_ui = :select
+    conf.update.columns = [:user]
+    conf.actions.exclude :create, :delete
   end
 end
