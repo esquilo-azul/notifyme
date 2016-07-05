bbc1d4a40d9dad20a80f14134db0165d84f98c27
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-07-04 23:30:09 -0300

    Move EacBase::EventManager para EventsManager. Refs #408.

--- init.rb
+++ init.rb
@@ -21,10 +21,10 @@ Redmine::Plugin.register :notifyme do
 end
 
 Rails.configuration.to_prepare do
-  EacBase::EventManager.add_listener(Issue, :create, 'Notifyme::Events::Issue::Create')
-  EacBase::EventManager.add_listener(Issue, :update, 'Notifyme::Events::Issue::Update')
-  EacBase::EventManager.add_listener(Repository, :receive, 'Notifyme::Events::Repository::Receive')
-  EacBase::EventManager.add_listener(TimeEntry, :create, 'Notifyme::Events::TimeEntry::Create')
-  EacBase::EventManager.add_listener(TimeEntry, :delete, 'Notifyme::Events::TimeEntry::Delete')
-  EacBase::EventManager.add_listener(TimeEntry, :update, 'Notifyme::Events::TimeEntry::Update')
+  EventsManager.add_listener(Issue, :create, 'Notifyme::Events::Issue::Create')
+  EventsManager.add_listener(Issue, :update, 'Notifyme::Events::Issue::Update')
+  EventsManager.add_listener(Repository, :receive, 'Notifyme::Events::Repository::Receive')
+  EventsManager.add_listener(TimeEntry, :create, 'Notifyme::Events::TimeEntry::Create')
+  EventsManager.add_listener(TimeEntry, :delete, 'Notifyme::Events::TimeEntry::Delete')
+  EventsManager.add_listener(TimeEntry, :update, 'Notifyme::Events::TimeEntry::Update')
 end
