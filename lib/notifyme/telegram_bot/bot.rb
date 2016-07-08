9a29e5839a8185f0ae7bd11e6ed3612e4817590a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-07-08 19:08:10 -0300

    Stub para testes com envio de mensagens Telegram. Refs 409.

--- lib/notifyme/telegram_bot/bot.rb
+++ lib/notifyme/telegram_bot/bot.rb
@@ -11,59 +11,11 @@ module Notifyme
         end
 
         def send_message(content_type, content, chat_ids)
-          if content_type == :plain
-            send_plain(content, chat_ids)
-          elsif content_type == :html
-            send_html(content, chat_ids)
+          if Rails.env.test?
+            ::Notifyme::TelegramBot::Senders::Fake.send_message(content_type, content, chat_ids)
           else
-            raise "Unknown content type: \"#{data[:content_type]}\""
+            ::Notifyme::TelegramBot::Senders::Real.send_message(content_type, content, chat_ids)
           end
-        end
-
-        private
-
-        def send_plain(plain_text, chat_ids)
-          run do |bot|
-            chat_ids.each do |chat_id|
-              bot.api.sendMessage(chat_id: chat_id, text: plain_text)
-            end
-          end
-        end
-
-        def send_html(html, chat_ids)
-          file_id = nil
-          chat_ids.each do |chat_id|
-            if file_id
-              send_photo_by_file_id(file_id, chat_id)
-            else
-              r = send_html_photo(html, chat_id)
-              file_id = r['result']['photo'][-1]['file_id'] if r['ok']
-            end
-          end
-        end
-
-        def send_html_photo(html, chat_id = nil)
-          send_photo(html_to_image_file(html), chat_id)
-        end
-
-        def send_photo(photo, chat_id)
-          photo = Faraday::UploadIO.new(File.expand_path(photo.to_s), nil) unless
-          photo.is_a?(Faraday::UploadIO)
-          run do |bot|
-            bot.api.sendPhoto(chat_id: chat_id, photo: photo)
-          end
-        end
-
-        def send_photo_by_file_id(file_id, chat_id)
-          run do |bot|
-            bot.api.sendPhoto(chat_id: chat_id, photo: file_id)
-          end
-        end
-
-        def html_to_image_file(html)
-          kit = IMGKit.new(html, quality: 50, width: 600)
-          file = Tempfile.new(['development-helper-image', '.png'])
-          Faraday::UploadIO.new(kit.to_file(file.path), nil)
         end
       end
     end
