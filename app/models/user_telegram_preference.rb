88d805e536ecb70018eeaea76128d54d9e28accf
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-04-11 17:24:16 -0300

    Refact UserTelegramPreference.

--- app/models/user_telegram_preference.rb
+++ app/models/user_telegram_preference.rb
@@ -1,37 +1,41 @@
 class UserTelegramPreference
   include ActiveModel::Model
 
+  PREFS = {
+    no_self_notified: {
+      get: 'false',
+      set: 'value.present? && value != \'0\''
+    },
+    issues: {
+      get: '\'only_my_events\'',
+      set: 'value'
+    },
+    issues_project_ids: {
+      get: '[]',
+      set: 'value.select(&:present?).map(&:to_i)'
+    }
+  }.freeze
+
   class << self
     def issues_values
       ::User::MAIL_NOTIFICATION_OPTIONS.map(&:first)
     end
   end
 
-  PREFS = %i(no_self_notified issues issues_project_ids).freeze
-
   attr_accessor :user
 
   validates :issues, presence: true, inclusion: { in: issues_values }
 
-  def save
-    user.pref[:telegram] = Hash[PREFS.map { |k| [k, send(k)] }]
-    user.pref.save!
-  end
+  PREFS.each do |k, v|
+    class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+      def #{k}
+        telegram_pref_get(__method__, #{v[:get]})
+      end
 
-  def no_self_notified
-    telegram_pref_get(__method__, false)
-  end
-
-  def no_self_notified=(value)
-    telegram_pref_set(__method__, value.present? && value != '0')
-  end
-
-  def issues
-    telegram_pref_get(__method__, 'only_my_events')
-  end
-
-  def issues=(value)
-    telegram_pref_set(__method__, value)
+      def #{k}=(value)
+        telegram_pref_set(__method__, #{v[:set]})
+      end
+    RUBY_EVAL
   end
 
   def issues_options
@@ -39,12 +43,9 @@ class UserTelegramPreference
     user.valid_notification_options.collect { |o| [::I18n.t(o.last), o.first] }
   end
 
-  def issues_project_ids
-    telegram_pref_get(__method__, [])
-  end
-
-  def issues_project_ids=(value)
-    telegram_pref_set(__method__, value.select(&:present?).map(&:to_i))
+  def save
+    user.pref[:telegram] = Hash[PREFS.keys.map { |k| [k, send(k)] }]
+    user.pref.save!
   end
 
   private
@@ -54,6 +55,7 @@ class UserTelegramPreference
   end
 
   def telegram_user_pref_get(name, default_value)
+    raise 'Attribute "user" is blank' if user.blank?
     if user.pref[:telegram] && user.pref[:telegram][name]
       user.pref[:telegram][name]
     else
