8085a9bee4045434a37b6da4fe7f563507c98836
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-08-07 16:22:50 -0300

    Update plugin "redmine_events_manager" to '0.4.2'.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -2,5 +2,5 @@
 
 Redmine::Plugin.post_register :notifyme do
   # Source: https://github.com/esquilo-azul/redmine_events_manager
-  requires_redmine_plugin(:redmine_events_manager, version_or_higher: '0.4.1')
+  requires_redmine_plugin(:redmine_events_manager, version_or_higher: '0.4.2')
 end
