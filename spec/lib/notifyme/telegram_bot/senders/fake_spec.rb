1966565e4ca6452b720ef63986ef4a1b2b1be8cc
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-06 16:34:55 -0300

    RSpec.describe(Notifyme::TelegramBot::Senders::Fake): convert from Notifyme::TelegramBot::Senders::FakeTest.

---
+++ spec/lib/notifyme/telegram_bot/senders/fake_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe ::Notifyme::TelegramBot::Senders::Fake do
+  context 'when first message is send' do
+    before do
+      ::Notifyme::TelegramBot::Bot.send_message(:plain, 'Test 1!', [12_345_678])
+    end
+
+    it do
+      expect(described_class.messages.last).to eq(
+        { content_type: :plain, content: 'Test 1!', chat_ids: [12_345_678] }
+      )
+    end
+  end
+
+  context 'when second message is send' do
+    before do
+      ::Notifyme::TelegramBot::Bot.send_message(:html, '<b>Test 2!</b>', [])
+    end
+
+    it do
+      expect(described_class.messages.last).to eq(
+        { content_type: :html, content: '<b>Test 2!</b>', chat_ids: [] }
+      )
+    end
+  end
+end
