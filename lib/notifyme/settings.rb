f97c8f267d822d941e81b24336238dbfb7c39bc1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-01 22:41:08 -0300

    Notifyme::Settings.setting_value: encapsulate Setting.plugin_notifyme.

--- lib/notifyme/settings.rb
+++ lib/notifyme/settings.rb
@@ -4,23 +4,23 @@ module Notifyme
   class Settings
     class << self
       def telegram_bot_default_chat_id
-        if Setting.plugin_notifyme['telegram_bot_default_chat_id'].blank?
+        if setting_value('telegram_bot_default_chat_id').blank?
           raise 'Chave do Bot Telegram não foi atribuída. Acesse /settings/plugin/notifyme para ' \
             'atribuir uma.'
         end
-        Setting.plugin_notifyme['telegram_bot_default_chat_id']
+        setting_value('telegram_bot_default_chat_id')
       end
 
       def telegram_bot_name
-        Setting.plugin_notifyme[__method__]
+        setting_value(__method__)
       end
 
       def issue_create_event_notify
-        Setting.plugin_notifyme[__method__]
+        setting_value(__method__)
       end
 
       def issue_update_event_notify
-        Setting.plugin_notifyme[__method__]
+        setting_value(__method__)
       end
 
       def assignee_reminder_query
@@ -33,8 +33,12 @@ module Notifyme
 
       private
 
+      def setting_value(key)
+        ::Setting.plugin_notifyme[key]
+      end
+
       def setting_value_or_raise(key)
-        v = Setting.plugin_notifyme[key]
+        v = setting_value(key)
         return v if v.present?
 
         raise "Setting.plugin_notifyme['#{key}'] is blank. " \
