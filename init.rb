d9589f81305a643fab580692902c12697bee261e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-12-26 14:31:58 -0300

    config/initializers/000_patches.rb: absorb all patches requires from "init.rb".

--- init.rb
+++ init.rb
@@ -2,14 +2,6 @@
 
 require 'redmine'
 
-require 'notifyme/patches/issue_patch'
-require 'notifyme/patches/users_helper_patch'
-require 'notifyme/patches/repository_patch'
-require 'notifyme/patches/user_patch'
-require_dependency 'notifyme/hooks/add_my_email_extra_link'
-require_dependency 'notifyme/hooks/add_my_telegram_link'
-require_dependency 'notifyme/hooks/add_assets'
-
 Redmine::Plugin.register :notifyme do
   name 'Notify me'
   author ::EacRedmineBase0::AUTHOR
