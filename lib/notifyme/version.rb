a39f53b78920d2c439b1ba2bd3e9f4c54963fd86
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-06-25 14:11:12 -0300

    Bump to '0.4.11'.

--- lib/notifyme/version.rb
+++ lib/notifyme/version.rb
@@ -4,5 +4,5 @@ module Notifyme
   AUTHOR = 'Eduardo Henrique Bogoni'
   HOMEPAGE = 'https://github.com/esquilo-azul/notifyme'
   SUMMARY = 'Notificações.'
-  VERSION = '0.4.10'
+  VERSION = '0.4.11'
 end
